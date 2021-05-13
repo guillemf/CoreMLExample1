4ece583cb5c277d4a15ca3c80f707cb31ec63709
Guillem Fernandez <hello@guillem.cat>
2021-05-13 15:58:28 +0200

    Initial Commit

---
+++ CoreMLExample1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoreMLExample1
+//
+//  Created by Guillermo Fernandez on 13/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CoreMLExample1/CoreMLExample1App.swift
@@ -0,0 +1,17 @@
+//
+//  CoreMLExample1App.swift
+//  CoreMLExample1
+//
+//  Created by Guillermo Fernandez on 13/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct CoreMLExample1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
